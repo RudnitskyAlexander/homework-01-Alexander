32f601cbc8a8120fd571c0fcd3c0c3bb9927431a
RudnitskyAlexander <108361831+RudnitskyAlexander@users.noreply.github.com>
2022-06-28 20:49:09 +0300

    урок 01

---
+++ Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Begin1◦
+        //. Дана сторона квадрата a. Найти его периметр P = 4·a.
+//ввод пили присвоение исходных данных
+
+//        int a = 5;
+//        вычисления результата
+//        int P = 4 * a;
+////        вывод результата на экран
+//        System.out.println(P);
+////        Дана сторона квадрата a. Найти его площадь S = a
+//2
+//.
+//        int S = a * a;
+//        System.out.println(S);
+////        Begin3◦
+////. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+////        периметр P = 2·(a + b).
+//
+//        int b = 6;
+//        int S1 = a * b;
+//        int P1 = 2 * (a + b);
+//        System.out.println("У квадрата со стороной " + a + " стороной " + b + " площадь = " + S1);
+//        System.out.println("У квадрата со стороной " + a + " стороной " + b + " периметр = " + P1);
+
+//        Begin4◦
+//. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
+//        значения π использовать 3.14.
+
+//        int d = 10;
+//        final double pi = 3.14;
+//        double L = pi * d;
+//        System.out.println("У круга с диаметром " + d + " длина окружности = " + L);
+
+//        Begin5◦
+//. Дана длина ребра куба a. Найти объем куба V = a3 и площадь его поверхности S = 6·a2
+
+//        int a=3;
+//        int V=a*a*a;
+//        int S=6*a*a;
+//        System.out.println(V);
+//        System.out.println(S);
+//
+//        Begin6◦
+//. Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти
+//        его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
+
+//        int a = 3;
+//        int b = 4;
+//        int c = 5;
+//        int V = a * b * c;
+//        int S = 2 * (a * b + b * c + a * c);
+//
+//        System.out.println(V);
+//        System.out.println(S);
+//
+//        Begin7◦
+//. Найти длину окружности L и площадь круга S заданного радиуса R:
+//        L = 2·π·R, S = π·R2.
+//        int R = 5;
+//        final double pi = 3.14;
+//        double L = 2 * pi * R;
+//        double S = pi * R*R;
+//        System.out.println(L);
+//        System.out.println(S);
+
+        //  Begin8◦ .Даны два числа a и b.Найти их среднее арифметическое:(a + b) / 2.
+
+        int a = 5;
+        int b = 6;
+        int d = 2;
+//        double c = (double) (a + b) / d;
+//        double c = (a + b) / (double) d;
+        double c = (a + b) / 2.0;
+        System.out.println(c);
+
+
+    }
+}
