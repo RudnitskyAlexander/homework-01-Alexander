b90b2da63302067962b51368bca154b18cc02e62
RudnitskyAlexander <108361831+RudnitskyAlexander@users.noreply.github.com>
2022-07-05 21:41:04 +0300

    lesson 003

--- Main.java
+++ Main.java
@@ -1,165 +1,254 @@
 package com.company;
 
-import javax.imageio.ImageTranscoder;
+import java.util.Scanner;
 
+// Занятие 3. логические операторы.
 public class Main {
-
     public static void main(String[] args) {
-
-        // Begin1◦
-        //. Дана сторона квадрата a. Найти его периметр P = 4·a.
-//ввод пили присвоение исходных данных
-
-//        int a = 5;
-//        вычисления результата
-//        int P = 4 * a;
-////        вывод результата на экран
-//        System.out.println(P);
-////        Дана сторона квадрата a. Найти его площадь S = a
-//2
-//.
-//        int S = a * a;
-//        System.out.println(S);
-////        Begin3◦
-////. Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
-////        периметр P = 2·(a + b).
+//  Boolean1◦
+//. Дано целое число A. Проверить истинность высказывания: «Число A является положительным».
 //
-//        int b = 6;
-//        int S1 = a * b;
-//        int P1 = 2 * (a + b);
-//        System.out.println("У квадрата со стороной " + a + " стороной " + b + " площадь = " + S1);
-//        System.out.println("У квадрата со стороной " + a + " стороной " + b + " периметр = " + P1);
-
-//        Begin4◦
-//. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
-//        значения π использовать 3.14.
-
-//        int d = 10;
-//        final double pi = 3.14;
-//        double L = pi * d;
-//        System.out.println("У круга с диаметром " + d + " длина окружности = " + L);
-
-//        Begin5◦
-//. Дана длина ребра куба a. Найти объем куба V = a3 и площадь его поверхности S = 6·a2
-
-//        int a=3;
-//        int V=a*a*a;
-//        int S=6*a*a;
-//        System.out.println(V);
-//        System.out.println(S);
+//        Scanner in = new Scanner(System.in);
 //
-//        Begin6◦
-//. Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти
-//        его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
-
-//        int a = 3;
-//        int b = 4;
-//        int c = 5;
-//        int V = a * b * c;
-//        int S = 2 * (a * b + b * c + a * c);
+//        int a = in.nextInt();
+//        boolean res =a>0;
+//        System.out.println(res);
 //
-//        System.out.println(V);
-//        System.out.println(S);
+//  Boolean2◦
+//. Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».
+
+//        Scanner in = new Scanner(System.in);
 //
-//        Begin7◦
-//. Найти длину окружности L и площадь круга S заданного радиуса R:
-//        L = 2·π·R, S = π·R2.
-//        int R = 5;
-//        final double pi = 3.14;
-//        double L = 2 * pi * R;
-//        double S = pi * R*R;
-//        System.out.println(L);
-//        System.out.println(S);
-
-        //  Begin8◦ .Даны два числа a и b.Найти их среднее арифметическое:(a + b) / 2.
+//        int a = in.nextInt();
+//        boolean res = a % 2 != 0;
+//        System.out.println(res);
 //
-//        int a = 5;
-//        int b = 6;
-//        int d = 2;
-//        double c = (double) (a + b) / d;
-//        double c = (a + b) / (double) d;
-//        double c = (a + b) / 2.0;
-//        System.out.println(c);
-//        Begin9◦
-//. Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их произведения: √
-//        a·b.
-//double a = 3.45;
-//double b=7.2;
-//double g_avg = Math.sqrt(a*b);
-//System.out.println(g_avg);
-//        Begin10◦
-//.Даны два ненулевых числа.Найти сумму, разность, произведение и
-//        частное их квадратов.
+//  Boolean3◦
+//. Дано целое число A. Проверить истинность высказывания: «Число A является четным».
 
-//        double a = 35.75;
-//        double b = 45.35;
+//        Scanner in = new Scanner(System.in);
 //
-//        double symm = a * a + b * b;
-//        double div = a * a - b * b;
-//        double mult = a * a * b * b;
-//        double chastn = (a * a) / (b * b);
-//        System.out.println(symm);
-//        System.out.println(div);
-//        System.out.println(mult);
-//        System.out.println(chastn);
-
-//        Begin11◦
-//. Даны два ненулевых числа. Найти сумму, разность, произведение и
-//        частное их модулей
-
-//        int a = -5;
-//        int b = 24;
-//        a = Math.abs(a);
-//        b = Math.abs(b);
-//        double s=a+b;
-//        double symm = a + b;
-//        System.out.println("Сумма модулей" + (int) s);
-//        double div = a - b;
-//        double mult = a * b;
-//        double chastn = (double) a / b;
-//        System.out.println("Сумма модулей" + (int) symm);
-//        System.out.println(div);
-//        System.out.println(mult);
-//        System.out.println(chastn);
-//        Begin22◦
-//. Поменять местами содержимое переменных A и B и вывести новые
-//        значения A и B.
-
-//        int a = 3;
-//        int b = 10;
-//        int c = a;
-//        a = b;
-//        b = c;
-//        System.out.println("a= "+a);
-//        System.out.println("b= "+b);
+//        int a = in.nextInt();
+//        boolean res = a % 2 == 0;
+//        System.out.println(res);
 //
-/*        Begin12◦
-. Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P:
-        c =√a2+b2
-    , P = a + b + c.*/
-//        double a = 5;
-//        double b = 6;
-//        double c = 7;
+//  Boolean4◦
+//. Даны два целых числа: A, B. Проверить истинность высказывания:
+//«Справедливы неравенства A > 2 и B ≤ 3».
 //
-//        double gipotenusa = Math.sqrt(a * a + b * b);
-//        System.out.println("гипотенуза " + gipotenusa);
-//        double perimetr = a + b + c;
-//        System.out.println("Периметр " + perimetr);
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.println("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = a > 2 && b <= 3;
+//        System.out.println("Результат: "+res);
 
-//        Begin13◦
-//.Даны два круга с общим центром и радиусами R1 и R2(R1 > R2).
-//                Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний
-//        радиус которого равен R1, а внутренний радиус равен R2:
-//        S1 = π·(R1) 2
-//                , S2 = π·(R2) 2
-//                , S3 = S1 −S2.
-//                В качестве значения π использовать 3.14.
+//  Boolean5◦
+//. Даны два целых числа: A, B. Проверить истинность высказывания:
+//«Справедливы неравенства A ≥ 0 или B < −2».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.println("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = a >= 0 || b < -2;
+//        System.out.println("Результат: " + res);
+
+//  Boolean6◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Справедливо двойное неравенство A < B < C».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.println("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = a < b && b < c;
+//        System.out.println("Результат: " + res);
 
 
+//  Boolean7◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Число B находится между числами A и C».
 
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.println("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = (a < b && b < c) || (c < b && b < a);
+//        System.out.println("Результат: " + res);
+
+//  Boolean8◦
+//. Даны два целых числа: A, B. Проверить истинность высказывания:
+//«Каждое из чисел A и B нечетное».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = a % 2 != 0 && b % 2 != 0;
+//        System.out.println("Результат: " + res);
+
+//  Boolean9◦
+// Даны два целых числа: A, B. Проверить истинность высказывания:
+//«Хотя бы одно из чисел A и B нечетное».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = (a % 2 != 0) || (b % 2 != 0);
+//        System.out.println("Результат: " + res);
+
+//  Boolean10◦
+//  Даны два целых числа: A, B. Проверить истинность высказывания: «Ровно одно из чисел A и B нечетное».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = (a % 2 != 0 && b % 2 == 0) || (a % 2 == 0 && b % 2 != 0);
+////        a % 2 != 0 ^ b % 2 != 0
+//        System.out.println("Результат: " + res);
+
+//  Boolean11
+//. Даны два целых числа: A, B. Проверить истинность высказывания: «Числа A и B имеют одинаковую четность».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        boolean res = (a % 2 != 0 && b % 2 != 0) || (a % 2 == 0 && b % 2 ==0);
+////        отстатки от деления на 2 равны
+////        boolean res = (a%2)==(b%2);
+//        System.out.println("Результат: " + res);
+
+
+//  Boolean12◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Каждое из чисел A, B, C положительное».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = a >= 0 && b >= 0 && c >= 0;
+//        System.out.println("Результат: " + res);
+
+//  Boolean13◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Хотя бы одно из чисел A, B, C положительное».
+//
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = a >= 0 || b >= 0 || c >= 0;
+//        System.out.println("Результат: " + res);
+
+//  Boolean14◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Ровно одно из чисел A, B, C положительное».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = (a > 0 && b < 0 && c < 0) || (a < 0 && b > 0 && c < 0) || (a < 0 && b < 0 && c > 0);
+////        a > 0 ^ b > 0^c>0
+//        System.out.println("Результат: " + res);
+
+//        Boolean15◦
+//. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Ровно два из чисел A, B, C являются положительными».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = (a > 0 && b > 0 && c < 0) || (a < 0 && b > 0 && c > 0) || (a > 0 && b < 0 && c > 0);
+//        System.out.println("Результат: " + res);
+
+//  Boolean16◦
+//. Дано целое положительное число. Проверить истинность высказывания: «Данное число является четным двузначным».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        boolean res = (a % 2 == 0) && (a >= 10 && a <= 99);
+//        System.out.println("Результат: " + res);
+
+//  Boolean18◦
+//. Проверить истинность высказывания: «Среди трех данных целых чисел есть хотя бы одна пара совпадающих»
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = (a == b) || (b == c) || (a == c);
+//        System.out.println("Результат: " + res);
+//
+//  Boolean19◦
+//. Проверить истинность высказывания: «Среди трех данных целых
+//        чисел есть хотя бы одна пара взаимно противоположных».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите значение числа А ");
+//        int a = in.nextInt();
+//        System.out.print("Введите значение числа В ");
+//        int b = in.nextInt();
+//        System.out.println("Введите значение числа C ");
+//        int c = in.nextInt();
+//        boolean res = (a == -b) || (b == -c) || (a == -c);
+//        System.out.println("Результат: " + res);
+
+//  Boolean20◦
+//. Дано трехзначное число. Проверить истинность высказывания:
+//«Все цифры данного числа различны».
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите трехзначное число ");
+//        int a = in.nextInt();
+//        int sot = a / 100;
+//        int des = (a / 10) % 10;  // (a%100)/10;
+//        int ed = a % 10;
+//        boolean res = (sot != des) && (des != ed) && (sot != ed);
+//        System.out.println("Результат: " + res);
+
+//  Boolean21◦
+//. Дано трехзначное число. Проверить истинность высказывания:
+//«Цифры данного числа образуют возрастающую последовательность». от сотен к единице
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите трехзначное число ");
+        int a = in.nextInt();
+        int sot = a / 100;
+        int des = (a / 10) % 10;  // (a%100)/10;
+        int ed = a % 10;
+        boolean res = (sot <des) && (des <ed) && (sot != ed);
+        System.out.println("Результат: " + res);
 
 
 
     }
 }
-
